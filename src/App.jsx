988f725b75a25c0967ba0a8dfffbdf8b501beba9
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-12-22 20:09:38 -0600

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -11,4 +10,6 @@ function App() {
     </div>
   );
 }
+
+
 export default App;
