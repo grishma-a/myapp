d7aaf8a26a2d5e577613b4acd35b13394fd33336
grishma-a <108956544+grishma-a@users.noreply.github.com>
2024-12-22 19:52:09 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Grishmas App!</h1>
+      <p>Start building something awesome 🚀</p>
+    </div>
+  );
+}
+export default App;
