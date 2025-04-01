2a74e83fd7bbbc76a6c441e4251463ff6814fc87
grishma-a <108956544+grishma-a@users.noreply.github.com>
2025-03-31 19:52:27 -0500

    fixed a lot

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import './App.css'
 import React, { useState } from 'react';
 
-function MyButton() {
-  return (
-    <button>I'm grishi button</button>
-  );
-}
+
 //export default MyButton;
 //The export default keywords specify the main component in the file.
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
+
 
 export default function MyApp() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+
+
     </div>
   );
 }
