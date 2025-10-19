1fed6bc0216e196964c158fb8f086c3d67bb0a9c
grishma-a <108956544+grishma-a@users.noreply.github.com>
2025-10-18 20:32:32 -0500

    update vie cong

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myapp/', // This should match your repository name
 })
